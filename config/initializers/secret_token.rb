0db6dfda5e7d6c4360872f42ca35caa185aaf15c
Matthew Martone <matt@clixlocal.net>
2013-08-03 11:07:13 -0400

    second commit with working users and microposts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7fe79a1656c840869bd1bafded62762a1d9f25edaf9bf9a2e7c12ac1eeba3030b1a9cf904e386b700fee80e9a69c2c276433ea8584c8708130bde153f6a57f99'
